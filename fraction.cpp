f2e5db1b64cabec51e0d818afe6092b2df0571ba
Ceweiss42 <ceweiss7@gmail.com>
2022-02-28 11:43:06 -0800

    finalProduct push
    finished lab 6

--- fraction.cpp
+++ fraction.cpp
@@ -4,17 +4,6 @@
 #include "fraction.h"
 using namespace std;
 
-/**************************************************************
-
- * The parameter constructor gets values for the numerator    *
-
- * and denominator, initializes the object, and normalizes the*
-
- * value of the numerator and the denominator according to the*
-
- * conditions defined in the class invariant. If denominator is 0, then set it to 1.             *
-
- **************************************************************/
 Fraction::Fraction(int num, int den)
 {
     numer = num;
@@ -25,91 +14,53 @@ Fraction::Fraction(int num, int den)
         denom = 1;
     }
 }
+
+
+Fraction::Fraction()
+{
+    numer = 0;
+    denom = 1;
+}
  
-
-/**************************************************************
-
- * The default constructor creates a fraction as 0/1. It does *
-
- * not need validation.                                       *
-
- **************************************************************/
- 
-
-/**************************************************************
-
- *The copy constructor creates a new fraction from an exisiting*
-
- * object. It does not need normalization because the source  *
-
- * object is already normalized.                              *
-
- **************************************************************/
-
-
-/**************************************************************
-
- * The destructor simply cleans up a fraction for recycling.  *
-
- **************************************************************/
-
+Fraction::Fraction(const Fraction& fract)
+{
+    Fraction (fract.numer, fract.denom);
+}
 
 Fraction :: ~Fraction ()
 {
-  //test
 }
  
 
-/**************************************************************
 
- * The getNumer function is an accessor function returning the*
-
- * numerator of the host object. It needs the const modifier  *
-
- **************************************************************/
+int Fraction::getNumer() const
+{
+    return numer; 
+}
 
 
-/**************************************************************
+int Fraction::getDenom() const
+{
+    return denom;
+}
 
- *The getDenum function is an accessor function returning the *
+void Fraction::print() const
+{
+    cout << numer << "/" << denom << endl;
+}
 
- *denominator of the host object. It needs the const modifier.*
+void Fraction::setNumer(int num)
+{
+    numer = num;
+    Fraction::normalize();
+}
 
- **************************************************************/
+void Fraction::setDenom(int den)
+{
+    denom = den;
+    Fraction::normalize();
+}
 
-
-/**************************************************************
-
- * The print function is an  accessor function with a side    *
-
- * effect that displays the fraction object in the form x/y.  *
-
- **************************************************************/
-
-
-/**************************************************************
-
- *The setNumer is a mutator function the changes the numerator*
-
- *of an existing object. The object needs normalization.      *
-
- **************************************************************/
-
-
-/**************************************************************
-
- * The setDenom is a mutator function that changes denominator*
-
- * of an existing object. The object needs normalization.     *
-
- **************************************************************/
-
-
-/**************************************************************
-
- * Normalize function takes care of three fraction invariants *      
-
- **************************************************************/
 
  
 
@@ -133,15 +84,6 @@ void Fraction :: normalize ()
   // Dividing numerator and denominator by gcd
 
 }
-
- 
-/**************************************************************
-
- * The gcd function finds the greatest common divisor between *
-
- * the numerator and the denominator.                         *
-
- **************************************************************/
 
  
 int Fraction :: gcd (int n, int m)
